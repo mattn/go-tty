49bd0b2a21bc7820d0fd8966bc3b76095d336988
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-06-04 21:39:43 +0900

    add ReadPassword

--- tty.go
+++ tty.go
@@ -2,6 +2,7 @@ package tty
 
 import (
 	"os"
+	"unicode"
 )
 
 func New() (*TTY, error) {
@@ -26,4 +27,31 @@ func (tty *TTY) Input() *os.File {
 
 func (tty *TTY) Output() *os.File {
 	return tty.output()
+}
+
+func (tty *TTY) ReadPassword() (string, error) {
+	rs := []rune{}
+loop:
+	for {
+		r, err := tty.readRune()
+		if err != nil {
+			return "", err
+		}
+		switch r {
+		case 13:
+			break loop
+		case 8:
+			if len(rs) > 0 {
+				rs = rs[:len(rs)-1]
+				tty.Output().WriteString("\b \b")
+			}
+		default:
+			if unicode.IsPrint(r) {
+				rs = append(rs, r)
+				tty.Output().WriteString("*")
+			}
+		}
+	}
+	tty.Output().WriteString("\n")
+	return string(rs), nil
 }
