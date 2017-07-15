0795b470ddcddd4ee227e9677a0db348185c3157
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-07-15 16:53:34 +0900

    add example

---
+++ example/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/mattn/go-tty"
+)
+
+func main() {
+	t, err := tty.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer t.Close()
+
+	for {
+		r, err := t.ReadRune()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if r == 0 {
+			continue
+		}
+		fmt.Printf("0x%X: %c\n", r, r)
+		if !t.Buffered() {
+			break
+		}
+	}
+}
