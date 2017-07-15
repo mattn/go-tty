d357f211017c6d1a9f4cc34212f874253a25bff8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-07-15 16:57:12 +0900

    signal should not be handled

--- tty_unix.go
+++ tty_unix.go
@@ -38,7 +38,7 @@ func open() (*TTY, error) {
 	}
 	newios := tty.termios
 	newios.Iflag &^= syscall.ISTRIP | syscall.INLCR | syscall.ICRNL | syscall.IGNCR | syscall.IXON | syscall.IXOFF
-	newios.Lflag &^= syscall.ECHO | syscall.ICANON | syscall.ISIG
+	newios.Lflag &^= syscall.ECHO | syscall.ICANON /*| syscall.ISIG*/
 	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(tty.in.Fd()), ioctlWriteTermios, uintptr(unsafe.Pointer(&newios)), 0, 0, 0); err != 0 {
 		return nil, err
 	}
